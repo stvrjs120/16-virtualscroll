391a9cfb231a559b3e0ae65b6083027c893f304e
Steven Rojas <stvrjs.k12@gmail.com>
2019-03-18 14:24:25 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VirtualComponent } from './components/virtual/virtual.component';
+
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { DragComponent } from './components/drag/drag.component';
+
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { PaisesComponent } from './components/paises/paises.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VirtualComponent,
+    DragComponent,
+    PaisesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ScrollingModule,
+    DragDropModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
